e3619d90bc31991dd263d8ba21df2f8d50081776
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-10-08 03:46:06 -0500

    Fixed Authentication

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,4 +1,4 @@
-const isAuthenicated = (req, res, next) => {
+const isAuthenticated = (req, res, next) => {
     if (req.session.user === undefined) {
         return res.status(401).json('You do not have any authorization to access this resource');
     }
@@ -6,5 +6,5 @@ const isAuthenicated = (req, res, next) => {
 };
 
 module.exports = { 
-    isAuthenicated 
+    isAuthenticated 
 }
--- routes/courses.js
+++ routes/courses.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 const coursesController = require('../controllers/courses');
 const { courseRules, validate } = require('../middleware/validate');
-const { isAuthenicated } = require('../middleware/authenticate');
+const { isAuthenticated } = require('../middleware/authenticate');
 
 router.get('/', coursesController.getAll); // Get all courses
 router.get('/:id', coursesController.getSingle); // Get a single course by ID
 
 // Apply validation rules to POST and PUT routes
-router.post('/', isAuthenicated, courseRules(), validate, coursesController.createCourse); // Create a new course
-router.put('/:id', isAuthenicated, courseRules(), validate, coursesController.updateCourse); // Update an existing course
+router.post('/', isAuthenticated, courseRules(), validate, coursesController.createCourse); // Create a new course
+router.put('/:id', isAuthenticated, courseRules(), validate, coursesController.updateCourse); // Update an existing course
 
-router.delete('/:id', isAuthenicated, coursesController.deleteCourse); // Delete a course
+router.delete('/:id', isAuthenticated, coursesController.deleteCourse); // Delete a course
 
 module.exports = router;
