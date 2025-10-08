86a56441d06b34a676f52165fac9bdacfce391ea
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-10-08 02:30:12 -0500

    Update to Authentication

--- routes/students.js
+++ routes/students.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 const studentsController = require('../controllers/students');
 const { studentRules, validate } = require('../middleware/validate');
-const { isAuthenicated } = require('../middleware/authenticate');
+const { isAuthenticated } = require('../middleware/authenticate');
 
 router.get('/', studentsController.getAll); // Get all students
 router.get('/:id', studentsController.getSingle); // Get a single student by ID
 
 // Apply validation rules to POST and PUT routes
-router.post('/', isAuthenicated, studentRules(), validate, studentsController.createStudent); // Create a new student
-router.put('/:id', isAuthenicated, studentRules(), validate, studentsController.updateStudent); // Update an existing student
+router.post('/', isAuthenticated, studentRules(), validate, studentsController.createStudent); // Create a new student
+router.put('/:id', isAuthenticated, studentRules(), validate, studentsController.updateStudent); // Update an existing student
 
-router.delete('/:id', isAuthenicated, studentsController.deleteStudent); // Delete a student
+router.delete('/:id', isAuthenticated, studentsController.deleteStudent); // Delete a student
 
 module.exports = router;
